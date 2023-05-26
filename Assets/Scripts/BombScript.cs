fabbb4ff69af133fec35d9796448475d805e9a4d
IsraelAlvesN <nicolauprog21@gmail.com>
2023-05-26 14:25:11 -0300

    Platforms and its destroy system created

--- Assets/Scripts/BombScript.cs
+++ Assets/Scripts/BombScript.cs
@@ -6,6 +6,8 @@ public class BombScript : MonoBehaviour
 {
     [SerializeField] private float explosionDelay = 5f;
     [SerializeField] private GameObject explosionPrefab;
+    public float blastRadius = 6f;
+    public int blastDamage = 12;
 
     void Start()
     {
@@ -27,6 +29,33 @@ public class BombScript : MonoBehaviour
     private void Explode()
     {
         Instantiate(explosionPrefab, transform.position, explosionPrefab.transform.rotation);
+
+        //destroy platforms
+        Collider[] colliders = Physics.OverlapSphere(transform.position, blastRadius);
+
+        foreach(Collider collider in colliders)
+        {
+            GameObject hitObject = collider.gameObject;
+            if (hitObject.CompareTag("Platform"))
+            {
+                LifeScript lifeScript = hitObject.GetComponent<LifeScript>();
+                if(lifeScript != null)
+                {
+                    //gameObject distance to explosion
+                    float distance = (hitObject.transform.position - transform.position).magnitude;
+                    float distanceRate = Mathf.Clamp(distance / blastRadius, 0, 1);
+                    float damageRate = 1f - Mathf.Pow(distanceRate, 6);
+                    int damage = (int) Mathf.Ceil(damageRate * blastDamage);
+                    lifeScript.health -= damage;
+
+                    if (lifeScript.health <= 0)
+                    {
+                        Destroy(collider.gameObject);
+                    }
+                }
+            }
+        }
+
         Destroy(gameObject);
     }
 }
